df3be5cd830aaa38adff5a97dc1f96ac19b02b2a
FranklinTesla <dyzxr@qq.com>
2017-11-11 15:19:13 +0800

    omitted code

---
+++ src/sort/shell-sort.js
@@ -0,0 +1,27 @@
+/**
+ * 在js中可能由于计算h的问题，希尔排序反而更耗时间
+ * @param arr
+ * @returns {*}
+ */
+function sort(arr) {
+    let h = 1
+        , len = arr.length
+        , hi = len/3
+    while (h < hi) {
+        h = 3*h+1
+    }
+    while (h >= 1) {
+        for (let i = h;i < len;i++) {
+            for (let j = i;j >= h;j -= h) {
+                if (arr[j] > arr[j-h]) {
+                    const temp = arr[j]
+                    arr[j] = arr[j-1]
+                    arr[j-1] = temp
+                }
+            }
+        }
+        h = parseInt(h/3)
+    }
+    return arr
+}
+module.exports = sort
